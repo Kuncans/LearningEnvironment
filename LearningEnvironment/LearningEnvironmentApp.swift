4e8c678a137477c94548a8fdb1f93ce3edb0879f
Kuncans <akafauna@gmail.com>
2022-02-02 12:08:35 +0000

    Initial Commit

---
+++ LearningEnvironment/LearningEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningEnvironmentApp.swift
+//  LearningEnvironment
+//
+//  Created by Duncan Kent on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
